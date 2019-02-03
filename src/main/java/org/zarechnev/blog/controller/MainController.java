3f5ae7fa0f5eb3f4350496a1235c029ff89e6199
Ruslan A. Zarechnev <zarechnev.r@yandex.ru>
2019-02-03 21:56:51 +0700

    Some changes.

--- src/main/java/org/zarechnev/blog/controller/MainController.java
+++ src/main/java/org/zarechnev/blog/controller/MainController.java
@@ -4,14 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.zarechnev.blog.model.ArticleModel;
 import org.zarechnev.blog.repository.BlogArticleRepository;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.zarechnev.blog.constant.ControllerPath.ADMIN_URL_PATH;
 import static org.zarechnev.blog.constant.ControllerPath.ARTICLE_URL_PATH;
 
@@ -34,7 +33,7 @@ public class MainController {
      * @param model   the model
      * @return string
      */
-    @RequestMapping(value = "/", method = GET)
+    @GetMapping("/")
     public String main(HttpServletRequest request, Map<String, Object> model) {
         log.info("someone come to us with URL: " + request.getRequestURL());
         model.put("articles", msgRepo.findAll());
@@ -54,7 +53,7 @@ public class MainController {
      * @param model   the model
      * @return string
      */
-    @RequestMapping(value = ARTICLE_URL_PATH + "/{id}", method = GET)
+    @GetMapping(value = ARTICLE_URL_PATH + "/{id}")
     public String articleById(HttpServletRequest request, @PathVariable("id") long id, Map<String, Object> model) {
         ArticleModel article;
 
@@ -80,7 +79,7 @@ public class MainController {
      * @param request the request
      * @return string
      */
-    @RequestMapping(value = ADMIN_URL_PATH, method = GET)
+    @GetMapping(ADMIN_URL_PATH)
     public String adminPage(HttpServletRequest request){
         log.info("Someone come to us with URL: " + request.getRequestURL());
         return "administration";
@@ -92,7 +91,7 @@ public class MainController {
      * @param request the request
      * @return the string
      */
-    @RequestMapping(value = "/foundation", method = GET)
+    @GetMapping("/foundation")
     public String foundation(HttpServletRequest request) {
         log.info("Someone come to us with URL: " + request.getRequestURL());
         return "foundation";
