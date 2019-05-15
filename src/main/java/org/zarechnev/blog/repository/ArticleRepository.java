94d596bc04c0b915065d09b6b69e8c138cb82ceb
Ruslan Zarechnev <ruslan.zarechnev@fxclub.com>
2019-05-15 15:24:25 +0700

    Switch to PagingAndSortingRepository.

--- src/main/java/org/zarechnev/blog/repository/ArticleRepository.java
+++ src/main/java/org/zarechnev/blog/repository/ArticleRepository.java
@@ -1,10 +1,10 @@
 package org.zarechnev.blog.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.zarechnev.blog.entity.ArticleModel;
 
 /**
  * The interface Blog article repository.
  */
-public interface ArticleRepository extends CrudRepository<ArticleModel, Long> {
+public interface ArticleRepository extends PagingAndSortingRepository<ArticleModel, Long> {
 }
--- src/main/java/org/zarechnev/blog/repository/ArticleSectionRepository.java
+++ src/main/java/org/zarechnev/blog/repository/ArticleSectionRepository.java
@@ -1,10 +1,10 @@
 package org.zarechnev.blog.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.zarechnev.blog.entity.ArticleSectionModel;
 
 /**
  * The interface Article section repository.
  */
-public interface ArticleSectionRepository extends CrudRepository<ArticleSectionModel, Integer> {
+public interface ArticleSectionRepository extends PagingAndSortingRepository<ArticleSectionModel, Integer> {
 }
